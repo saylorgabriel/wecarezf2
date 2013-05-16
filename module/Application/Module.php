6ceb9542192d96e566807332d56b343806b3c687
saylor gabriel damasceno <saylor.damasceno@superlogica.com>
2013-05-16 13:31:18 -0300

    Novo projeto

--- module/Application/Module.php
+++ module/Application/Module.php
@@ -1,31 +1,18 @@
 <?php
-/**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
- * @copyright Copyright (c) 2005-2013 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- */
-
+ 
+# namespace no qual se encontra Module.php
 namespace Application;
-
-use Zend\Mvc\ModuleRouteListener;
-use Zend\Mvc\MvcEvent;
-
+ 
 class Module
 {
-    public function onBootstrap(MvcEvent $e)
-    {
-        $eventManager        = $e->getApplication()->getEventManager();
-        $moduleRouteListener = new ModuleRouteListener();
-        $moduleRouteListener->attach($eventManager);
-    }
-
+ 
+    # include de arquivo para outras configuracoes
     public function getConfig()
     {
         return include __DIR__ . '/config/module.config.php';
     }
-
+ 
+    # autoloader para namespaces
     public function getAutoloaderConfig()
     {
         return array(
@@ -36,4 +23,5 @@ class Module
             ),
         );
     }
+ 
 }
